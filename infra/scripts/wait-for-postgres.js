2decbbcbd62b40038e87391d85f82cb67a42dbb1
Jorge Malta Rodrigues de Oliveira <jorge_malta@outlook.com>
2026-07-02 18:23:36 +0000

    make `npm test` command more robust using `async-retry` and `orchestrator.js`

--- infra/scripts/wait-for-postgres.js
+++ infra/scripts/wait-for-postgres.js
@@ -10,7 +10,7 @@ function checkPostgres() {
       return;
     }
 
-    console.log("\n🟢 Postgres pornto e aceitando conexões!\n");
+    console.log("\n🟢 Postgres está pronto e aceitando conexões!\n");
   }
 }
 process.stdout.write("\n\n🔴 Aguardando postgres aceitar conexões ");
